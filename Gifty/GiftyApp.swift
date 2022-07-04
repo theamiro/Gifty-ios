51cb78dee1cd11a02aae12caded0899edd763b48
Michael Amiro <michael.odindo@azenia.com>
2022-07-04 15:34:52 +0300

    Initial Commit

---
+++ Gifty/GiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftyApp.swift
+//  Gifty
+//
+//  Created by Michael Amiro on 04/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
